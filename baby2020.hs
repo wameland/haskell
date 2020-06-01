009f4b6773ff70a1d84c39db6de0cde96babd229
Wade A <wadeamesbury@gmail.com>
2020-06-01 11:15:46 -0700

    Add files via upload

---
+++ baby2020.hs
@@ -0,0 +1,31 @@
+--LearnYouAHaskellForGreatGood					--
+--
+--filename:	 baby.hs					--
+--
+--By: 		Wade Daniel Amesbury	(DBA:ESGG)		--
+--Created:	4:37 AM 09/08/2019				--
+--edit:		5:07 PM 2019-09-15				--
+--edit:		11:58 AM 2019-10-05				--
+--edit:		11:12 AM 2020-06-01				--
+
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100 then x else x*2
+mainBabyFx a -> b -> a b
+
+
+--The rants of how a baby F(x) would think.
+--Why me? Why you? Why anything at all?  Is it I that doesn't      --
+--that makes no sense? Is it this machine that makes no sense   --
+--Like, what should I expeect when I wake up in a hospital bed
+--end up making friends with a perfect stranger and then being 
+--ridiculed by them about how you should really be enjoying   
+--myself in an environment in() which I am in(). The Toulomne river
+--is really great, the way I packed and the mental state I 
+--was in was not good. I feel like a lonesome fool left to play
+--a game one else is participating in any longer.
+--I am having a splendid time doing whatever I 
+--I feel like and especially now that I am feeling much better
+--				-END-				--
+
+
